e7c684ab274b0f3efa8320eb3f9a0afb9665a1ac
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-08-22 01:07:41 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -9,6 +9,11 @@ def second_challenge
       #your key/value pairs here!
       }, 
     :capulet => {
+       :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
       #your key/value pairs here!
       }}
 
